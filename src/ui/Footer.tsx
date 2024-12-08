f613cab95b5c5d3a5fac58261263860c509847f9
brani <branislannjemec@gmail.com>
2024-12-09 00:13:01 +0100

    change the color of footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-blue-600 text-white py-4">
+    <footer className="bg-gray-400 text-white py-4">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 FONsion Hotel. All rights reserved.</p>
       </div>
