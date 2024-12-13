7d7affa5e601975fdd0ec60769790674d2692f5a
njemecc <branislannjemec@gmail.com>
2024-12-13 18:22:02 +0100

    create api for fetching rooms

---
+++ src/services/apiRooms.ts
@@ -0,0 +1,28 @@
+
+import { Room } from "../features/Rooms/types";
+import axios from "axios"
+import { ErrorOrResponse } from "../types/globalTypes";
+import { backendUrl } from "./backendUrl";
+
+
+export async function getAllRooms():Promise<Room[]>{
+
+    const response = await axios.get<ErrorOrResponse<Room[]>>(`${backendUrl}/api/room`)
+    
+
+    if (response.data.isError) {
+        const errorMessage =
+          response.data.firstError?.description || "Unknown error occurred";
+        throw new Error(errorMessage);
+      }
+  
+      if (response.data.value) {
+        return response.data.value;
+      }
+  
+      throw new Error("Unexpected response format");
+    };
+
+
+
+
