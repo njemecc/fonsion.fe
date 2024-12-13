fd3b0eb66628b78b9bedf076d6d77ae573d2874f
njemecc <branislannjemec@gmail.com>
2024-12-13 18:23:01 +0100

    add tanstackQueryProvider and route to roomsPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,30 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router";
 import AppLayout from "./ui/AppLayout";
+import RoomsPage from "./pages/RoomsPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route path="/" element={<RoomsPage />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
