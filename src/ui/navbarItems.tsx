206456710caca72e7e89f98a6590c90bdd6c64a1
njemecc <branislannjemec@gmail.com>
2024-12-15 23:59:25 +0100

    change text color

--- src/ui/navbarItems.tsx
+++ src/ui/navbarItems.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router";
-import { Image } from "lucide-react";
 
 const transition = {
   type: "spring",
@@ -27,7 +26,7 @@ export const MenuItem = ({
     <div onMouseEnter={() => setActive(item)} className="relative ">
       <motion.p
         transition={{ duration: 0.3 }}
-        className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white"
+        className="cursor-pointer text-white hover:opacity-[0.9] dark:text-white"
       >
         {item}
       </motion.p>
@@ -69,7 +68,7 @@ export const Menu = ({
   return (
     <nav
       onMouseLeave={() => setActive(null)} // resets the state
-      className="relative rounded-full border border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-4 px-8 py-6 "
+      className="bg-appBrown text-white relative rounded-full border border-transparent dark:bg-black dark:border-white/[0.2] shadow-input flex justify-center space-x-4 px-8 py-6 "
     >
       {children}
     </nav>
