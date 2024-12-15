78501fdc5ef3bc65bd96ddf00dff58a70c10fd9a
njemecc <branislannjemec@gmail.com>
2024-12-15 23:58:03 +0100

    add color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,16 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
   	extend: {
+		
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		colors: {
+			appNavy:"#101730",
+			appBrown:"#B9A285",
+
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
