9d87ef8b3ce2365d608f5a3576b51a18ec7188ad
njemecc <branislannjemec@gmail.com>
2024-12-24 22:47:26 +0100

    add FilterRangeCalendar

--- src/features/Rooms/AllRoomCards.tsx
+++ src/features/Rooms/AllRoomCards.tsx
@@ -1,16 +1,27 @@
+import FilterRangeCalendar from "./FilterRangeCalendar";
 import { RoomCard } from "./RoomCard";
 import { useGetAllRooms } from "./useGetAllRooms";
 
 const AllRoomCards = () => {
   const { rooms, isLoading } = useGetAllRooms();
 
-  isLoading && <div>Loading...</div>;
+  if (isLoading) return <div>Loading...</div>;
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-24">
-      {rooms?.map((room) => (
-        <RoomCard key={room.id} room={room} />
-      ))}
+    <div className="container mx-auto px-4 mt-8">
+      {/* Sekcija za filter */}
+      <div className="flex justify-end mb-6">
+        <div className="p-4 bg-white shadow-md rounded-md w-full sm:w-auto">
+          <FilterRangeCalendar />
+        </div>
+      </div>
+
+      {/* Sekcija za kartice */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {rooms?.map((room) => (
+          <RoomCard key={room.id} room={room} />
+        ))}
+      </div>
     </div>
   );
 };
