ade4212930dbb7257d81c93b01da8295b6163b25
njemecc <branislannjemec@gmail.com>
2024-12-24 22:48:27 +0100

    add queries from url

--- src/features/Rooms/useGetAllRooms.ts
+++ src/features/Rooms/useGetAllRooms.ts
@@ -1,12 +1,20 @@
+import { useSearchParams } from "react-router"
 import { getAllRooms } from "../../services/apiRooms"
 import { useQuery } from "@tanstack/react-query"
 
 
 export function useGetAllRooms(){
+
+    const [searchParams] = useSearchParams();
+
+    const fromDate = searchParams.get("fromDate")
+    const toDate = searchParams.get("toDate")
+
+    console.log(fromDate, toDate)
     
     const {data : rooms, isLoading,error} = useQuery({
-        queryKey:["rooms"],
-        queryFn:getAllRooms
+        queryKey:["rooms",fromDate,toDate],
+        queryFn:() => getAllRooms(fromDate,toDate)
     })
 
     return {
