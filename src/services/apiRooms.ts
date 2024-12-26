53274f8b48ecbfefe3982b68957bf84c7386c0dc
njemecc <branislannjemec@gmail.com>
2024-12-26 22:15:39 +0100

    add paginaiton

--- src/services/apiRooms.ts
+++ src/services/apiRooms.ts
@@ -1,13 +1,20 @@
 
 import { Room } from "../features/Rooms/roomTypes";
 import axios from "axios"
-import { ErrorOrResponse } from "../types/globalTypes";
+import { ErrorOrResponse, PaginatedResult } from "../types/globalTypes";
 import { backendUrl } from "./backendUrl";
 
 
-export async function getAllRooms(fromDate:string | null,toDate:string | null):Promise<Room[]>{
+export async function getAllRooms(fromDate:string | null,toDate:string | null, page: number = 0, pageSize: number = 5):Promise<PaginatedResult<Room>>{
 
-    const response = await axios.get<ErrorOrResponse<Room[]>>(`${backendUrl}/api/room/${fromDate}/${toDate}`)
+ const params = new URLSearchParams()
+
+ if (fromDate) params.append("fromDate", fromDate);
+ if (toDate) params.append("toDate", toDate);
+ params.append("page", page.toString());
+ params.append("pageSize", pageSize.toString());
+
+    const response = await axios.get<ErrorOrResponse<PaginatedResult<Room>>>(`${backendUrl}/api/room`,{params})
     
 
     if (response.data.isError) {
