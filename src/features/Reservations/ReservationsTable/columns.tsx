acbb119bc8f54db2d4db6727f8998aa83f4bdbf9
njemecc <branislannjemec@gmail.com>
2025-01-09 22:58:11 +0100

    add images to reservations table

--- src/features/Reservations/ReservationsTable/columns.tsx
+++ src/features/Reservations/ReservationsTable/columns.tsx
@@ -1,8 +1,6 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { MdCancel } from "react-icons/md";
-
 import { ArrowUpDown, MoreHorizontal } from "lucide-react";
-
 import { Button } from "../../../components/ui/button";
 import {
   DropdownMenu,
@@ -14,36 +12,62 @@ import {
 } from "../../../components/ui/dropdown-menu";
 import { GetReservationByUserIdResponse } from "../reservationTypes";
 import { formatDate } from "date-fns";
+import { useCancelReservation } from "../hooks/useCancelReservation";
+
+const ReservationActions = ({ reservationId }: { reservationId: string }) => {
+  const { cancelReservation } = useCancelReservation();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem
+          onClick={() => navigator.clipboard.writeText(reservationId)}
+        >
+          Copy room name
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          {" "}
+          <Button onClick={() => cancelReservation(reservationId)}>
+            <MdCancel />
+            Cancel reservation
+          </Button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 export const columns: ColumnDef<GetReservationByUserIdResponse>[] = [
   {
     id: "actions",
     cell: ({ row }) => {
       const reservation = row.original;
+      return <ReservationActions reservationId={reservation.reservationId} />;
+    },
+  },
 
+  {
+    accessorKey: "images",
+    header: "Images",
+    cell: ({ row }) => {
+      const images = row.getValue("images") as string[];
+      const imageUrl = images[0];
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() =>
-                navigator.clipboard.writeText(reservation.roomName)
-              }
-            >
-              Copy room name
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <MdCancel /> Cancel reservation
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="flex items-center justify-center">
+          <img
+            src={imageUrl}
+            alt="Room"
+            className="h-14 w-24 object-cover rounded"
+          />
+        </div>
       );
     },
   },
@@ -56,14 +80,14 @@ export const columns: ColumnDef<GetReservationByUserIdResponse>[] = [
     header: "From Date",
     cell: ({ row }) => {
       const date = new Date(row.getValue("fromDate"));
-      return <div>{formatDate(date, "dd/MM/yyyy")}</div>; // Custom format like "DD/MM/YYYY"
+      return <div>{formatDate(date, "dd/MM/yyyy")}</div>;
     },
   },
   {
     accessorKey: "toDate",
     header: "To Date",
     cell: ({ row }) => {
-      const date = new Date(row.getValue("fromDate"));
+      const date = new Date(row.getValue("toDate"));
       return <div>{formatDate(date, "dd/MM/yyyy")}</div>;
     },
   },
@@ -88,7 +112,30 @@ export const columns: ColumnDef<GetReservationByUserIdResponse>[] = [
         currency: "USD",
       }).format(amount);
 
-      return <div className="text-right font-medium">{formatted}</div>;
+      return <div className=" font-medium">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "status",
+    cell: ({ row }) => {
+      const status = row.getValue("status");
+      const statusClass =
+        status === 0
+          ? "bg-red-500 text-white" // Canceled: red
+          : status === 1
+          ? "bg-green-500 text-white" // Active: green
+          : "bg-yellow-500 text-black"; // Pending: yellow
+
+      const statusText =
+        status === 0 ? "Canceled" : status === 1 ? "Active" : "Pending";
+
+      return (
+        <div
+          className={`inline-flex items-center px-3 py-1 rounded-full text-sm font-medium ${statusClass}`}
+        >
+          {statusText}
+        </div>
+      );
     },
   },
 ];
