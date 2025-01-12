e183b737d4b4f170e437d0156fac1704c9fdedb9
njemecc <branislannjemec@gmail.com>
2025-01-12 23:47:52 +0100

    add clerk provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import ReservationsPage from "./pages/ReservationsPage";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,53 +15,64 @@ const queryClient = new QueryClient({
   },
 });
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={true} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
-            <Route path="/" element={<RoomsPage />}></Route>
-            <Route path="/reservations" element={<ReservationsPage />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      <Toaster
-        position="bottom-right"
-        gutter={12}
-        containerStyle={{ margin: "8px" }}
-        toastOptions={{
-          success: {
-            duration: 3000,
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={true} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AppLayout />}>
+              <Route path="/" element={<RoomsPage />}></Route>
+              <Route
+                path="/reservations"
+                element={<ReservationsPage />}
+              ></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <Toaster
+          position="bottom-right"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+              style: {
+                backgroundColor: "green",
+                color: "white",
+                fontSize: "16px",
+                maxWidth: "500px",
+                padding: "16px 24px",
+                borderRadius: "8px",
+              },
+            },
+            error: {
+              duration: 5000,
+              style: {
+                backgroundColor: "red",
+                color: "white",
+                fontSize: "16px",
+                maxWidth: "500px",
+                padding: "16px 24px",
+                borderRadius: "8px",
+              },
+            },
             style: {
-              backgroundColor: "green",
-              color: "white",
               fontSize: "16px",
               maxWidth: "500px",
               padding: "16px 24px",
-              borderRadius: "8px",
             },
-          },
-          error: {
-            duration: 5000,
-            style: {
-              backgroundColor: "red",
-              color: "white",
-              fontSize: "16px",
-              maxWidth: "500px",
-              padding: "16px 24px",
-              borderRadius: "8px",
-            },
-          },
-          style: {
-            fontSize: "16px",
-            maxWidth: "500px",
-            padding: "16px 24px",
-          },
-        }}
-      />
-    </QueryClientProvider>
+          }}
+        />
+      </QueryClientProvider>
+    </ClerkProvider>
   );
 }
 
